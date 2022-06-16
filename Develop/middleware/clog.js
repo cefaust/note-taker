a243e1b0ae3a76a28a446d3e65f4ae5eb22d459d
Chris Faust <hello@chrisfaust.dev>
2022-06-15 20:39:04 -0700

    added a helper file and middleware file

---
+++ Develop/middleware/clog.js
@@ -0,0 +1,23 @@
+const clog = (req, res, next) => {
+  const fgCyan = '\x1b[36m';
+
+  switch (req.method) {
+    case 'GET': {
+      console.info(`📗${fgCyan}${req.method} request to ${req.path}`);
+      break
+    }
+    case 'POST': {
+      console.info(`📘${fgCyan}${req.method} request to ${req.path}`);
+      break
+    }
+    case 'Delete': {
+      console.info(`📙${fgCyan}${req.method} request to ${req.path}`);
+      break
+    }
+    default:
+      console.log(`${fgCyan}${req.method} request to ${req.path}`)
+  }
+  next();
+}
+
+exports.clog = clog;
